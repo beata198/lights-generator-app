7b96adf191ac02fcd1a00b629ddb066fad6dbe3f
beata198 <b.lublewska@gmail.com>
2023-07-09 16:56:08 +0200

    feat: create light

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import bulbTurnedOff from "/images/bulb.svg";
 import bulbBlue from "/images/light-blue.svg";
-import bulbGreen from "/images/light-green.svg";
-import bulbOrange from "/images/light-orange.svg";
-import bulbPink from "/images/light-pink.svg";
-import bulbRed from "/images/light-red.svg";
-import bulbYellow from "/images/light-yellow.svg";
+// import bulbGreen from "/images/light-green.svg";
+// import bulbOrange from "/images/light-orange.svg";
+// import bulbPink from "/images/light-pink.svg";
+// import bulbRed from "/images/light-red.svg";
+// import bulbYellow from "/images/light-yellow.svg";
 
 const SingleLight = () => {
   const [isOn, setIsOn] = useState(true);
